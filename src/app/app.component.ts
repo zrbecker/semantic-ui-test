de7ddb907239c8194b1310c5cfd800d9b2ab6b30
Zachary Becker <zrbecker@gmail.com>
2017-08-23 18:26:48 -0700

    component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare let jQuery: any;
 
 @Component({
   selector: 'app-root',
   template: `
-    <h1>
-      Welcome to {{title}}!!
-    </h1>
-    <router-outlet></router-outlet>
+  <h3>{{ title }}</h3>
+  <div class="ui search">
+    <div class="ui icon input">
+      <input class="prompt" type="text" placeholder="Search...">
+      <i class="search icon"></i>
+    </div>
+  </div>
   `,
   styles: []
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  content = [
+    { title: 'red' },
+    { title: 'green' },
+    { title: 'blue' }
+  ];
+
+  ngOnInit(): void {
+    jQuery('.ui.search').search({
+      source: this.content
+    });
+  }
 }
